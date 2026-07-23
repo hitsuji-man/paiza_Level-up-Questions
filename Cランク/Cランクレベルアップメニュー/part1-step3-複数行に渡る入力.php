c22db28f4e6ee0ace4075d4074fcf32761ad76f8
hitsuji-man <renton1q84@gmail.com>
2026-07-23 17:14:36 +0900

    part1-step3-複数行に渡る入力

---
+++ Cランク/Cランクレベルアップメニュー/part1-step3-複数行に渡る入力.php
@@ -0,0 +1,9 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $n = trim(fgets(STDIN));
+    for ($i = 1; $i <= $n; $i++) {
+         $number = trim(fgets(STDIN));
+         echo "$number\n";
+    }
+?>
