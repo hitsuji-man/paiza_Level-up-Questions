2c20b1f47577ffbac1ba263cbf2fe2c87b684693
hitsuji-man <renton1q84@gmail.com>
2026-08-14 22:08:04 +0900

    part1-step6-文字列の比較

---
+++ Cランク/条件式メニュー/part1-step6-文字列の比較.php
@@ -0,0 +1,10 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    [$S, $T] = explode(" ", trim(fgets(STDIN)));
+    if ($S === $T) {
+        echo "1\n";
+    } else {
+        echo "0\n";
+    }
+?>
