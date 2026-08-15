856dac6d05d71a4f36a17c3ad293fcb4ed107196
hitsuji-man <renton1q84@gmail.com>
2026-08-15 20:50:37 +0900

    part2-step1-AND演算子

---
+++ Cランク/条件式メニュー/part2-step1-AND演算子.php
@@ -0,0 +1,10 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    [$A, $B, $C, $D] = explode(" ", trim(fgets(STDIN)));
+    if ($A == $B && $C == $D) {
+        echo "1\n";
+    } else {
+        echo "0\n";
+    }
+?>
