66211f36fc93c3a76084fc8a7ef75d45920df3c2
hitsuji-man <renton1q84@gmail.com>
2026-09-13 20:10:33 +0900

    part8-stepFinal-文字列の抽出

---
+++ Cランク/Cランク・スキルチェック過去問題セット/part8-stepFinal-文字列の抽出.php
@@ -0,0 +1,21 @@
+<?php
+[$tag_start, $tag_end] = explode(" ", trim(fgets(STDIN)));
+$S = trim(fgets(STDIN));
+
+$offset = 0;
+
+while (($startPos = strpos($S, $tag_start, $offset)) !== false) {
+    // 開始タグの直後に移動
+    $textStart = $startPos + strlen($tag_start);
+    // 対応する終了タグを検索
+    $endPos = strpos($S, $tag_end, $textStart);
+    if ($endPos === false) {
+        break;
+    }
+    $text = substr($S, $textStart, $endPos - $textStart);
+    
+    echo $text === "" ? "<blank>\n" : $text . "\n";
+
+    // 終了タグの後から次の検索を開始
+    $offset = $endPos + strlen($tag_end);
+}
