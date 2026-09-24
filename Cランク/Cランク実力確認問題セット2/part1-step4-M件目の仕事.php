beed3169d3b27023652b26a7871773cfdd9936ea
hitsuji-man <renton1q84@gmail.com>
2026-09-24 18:48:35 +0900

    part1-step4-M件目の仕事

---
+++ Cランク/Cランク実力確認問題セット2/part1-step4-M件目の仕事.php
@@ -0,0 +1,26 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    [$N, $M] = explode(" ", trim(fgets(STDIN)));
+    $names = [];
+    for ($i = 0; $i < $N; $i++) {
+        $names[] = trim(fgets(STDIN));
+    }
+    
+    $job = [];
+    $key = 0;
+    for ($i = 0; $i < $M; $i++) {
+        // 名前は＄N個あるので$N回ごとに繰り返す($N=4の場合)
+        // $i.  : 0 1 2 3 4 5 6 7 8
+        // $key : 0 1 2 3 0 1 2 3 0
+        if ($i % $N == 0) {
+            $key = 0;
+        }
+        $name = $names[$key];
+        $job[$i] = $name;
+        $key++;
+    }
+    $number_M = $M - 1;
+    echo "$job[$number_M]\n";
+    // print_r($job);
+?>
